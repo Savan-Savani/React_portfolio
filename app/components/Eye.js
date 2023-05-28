f771f6e6c2dc1be4112181157a9091b263c6281e
Savan <savansavani111@gmail.com>
2023-05-28 15:40:45 -0400

    eye done

---
+++ app/components/Eye.js
@@ -0,0 +1,78 @@
+import React, { useEffect, useRef, useState } from "react";
+
+const Eye = () => {
+  const eyeRef = useRef(null);
+  const pupilRef = useRef(null);
+  const [eyePosition, setEyePosition] = useState({ x: 0, y: 0 });
+  const [relativeAngle, setRelativeAngle] = useState(0);
+
+  useEffect(() => {
+    const eyeElement = eyeRef.current;
+    const eyeRect = eyeElement.getBoundingClientRect();
+
+    const eyeCenterX = eyeRect.left + eyeRect.width / 2;
+    const eyeCenterY = eyeRect.top + eyeRect.height / 2;
+
+    setEyePosition({ x: eyeCenterX, y: eyeCenterY });
+  }, []);
+
+  useEffect(() => {
+    const handleMouseMove = (event) => {
+      const xPos = event.clientX;
+      const yPos = event.clientY;
+
+      const angle = findAngle(eyePosition.x, eyePosition.y, xPos, yPos);
+      setRelativeAngle(angle);
+
+      const xA = -32;
+      const yA = -32;
+      const distance = 50;
+
+      const angleRad = (angle * Math.PI) / 180;
+
+      const xB = xA + distance * Math.cos(angleRad);
+      const yB = yA + distance * Math.sin(angleRad);
+
+      const pupilElement = pupilRef.current;
+      if (pupilElement) {
+        pupilElement.style.transform = `translate(${xB}px, ${yB}px)`;
+      }
+    };
+
+    document.addEventListener("mousemove", handleMouseMove);
+
+    return () => {
+      document.removeEventListener("mousemove", handleMouseMove);
+    };
+  }, [eyePosition]);
+
+  const findAngle = (x1, y1, x2, y2) => {
+    const dx = x2 - x1;
+    const dy = y2 - y1;
+
+    const angleInRadians = Math.atan2(dy, dx);
+    const angleInDegrees = (angleInRadians * 180) / Math.PI;
+
+    return angleInDegrees;
+  };
+
+  return (
+    <div className="flex items-center justify-center w-96 h-96 bg-gradient-to-r from-cyan-500 to-teal-500 rounded-2xl">
+      <div
+        ref={eyeRef}
+        className="relative w-72 h-[45%] rounded-[80%] bg-white shadow-md mt-16"
+      >
+        <div className="absolute w-6 h-[40%] rounded-full bg-white transform rotate-[320deg] left-[10%] top-[-40%]"></div>
+        <div className="absolute w-6 h-[40%] rounded-full bg-white transform rotate-[40deg] left-[80%] top-[-40%] "></div>
+        <div className="absolute w-6 h-[40%] rounded-full bg-white transform rotate-[360deg] left-[45%] top-[-50%] "></div>
+
+        <div
+          ref={pupilRef}
+          className="w-16 h-16 rounded-full bg-black absolute top-1/2 left-1/2 "
+        ></div>
+      </div>
+    </div>
+  );
+};
+
+export default Eye;
--- app/page.js
+++ app/page.js
@@ -4,18 +4,24 @@ import { BsFillMoonStarsFill } from "react-icons/bs";
 import { AiFillGithub, AiFillLinkedin } from "react-icons/ai";
 import profileImg from "../public/profileImg.png";
 import { useState } from "react";
+import Eye from "./components/Eye";
 
 export default function Home() {
   const [darkMode, setDarkMode] = useState(false);
 
   return (
     <div className={`app ${darkMode ? "dark" : ""}`}>
-      <main className="px-10 bg-white dark:bg-gray-900 md:px-20 lg:px-40 ">
+      <main className="px-10 bg-[#fefaf6] dark:bg-gray-900 md:px-20 lg:px-40 ">
         <section className="min-h-screen">
           <nav className="py-10 mb-12 flex justify-between">
-            <h1 className="text-xl font-burtons dark:text-white">
-              Developed by Savan
-            </h1>
+            <div className="flex items-center">
+              <div className="relative bg-gradient-to-b from-teal-500 rounded-full mx-2 h-10 w-10 overflow-hidden md:h-10 md:w-10">
+                <Image src={profileImg} objectFit="cover" layout="fill" />
+              </div>
+              <h1 className="text-xl font-burtons dark:text-white">
+                Developed by Savan
+              </h1>
+            </div>
             <ul className="flex items-center">
               <li>
                 <BsFillMoonStarsFill
@@ -35,49 +41,77 @@ export default function Home() {
                   Resume
                 </a>
               </li>
+              <li>
+                <div className="text-5xl flex justify-center py-3 text-gray-600 dark:text-gray-400">
+                  <a
+                    className="mx-2"
+                    href="https://github.com/Savan-Savani"
+                    target="_blank"
+                  >
+                    <AiFillGithub />
+                  </a>
+
+                  <a
+                    target="_blank"
+                    href="https://www.linkedin.com/in/savansavani/"
+                  >
+                    <AiFillLinkedin />
+                  </a>
+                </div>
+              </li>
             </ul>
           </nav>
-          <div className="text-center p-10">
-            <h2 className="text-5xl py-2 text-teal-500 font-medium md:text-6xl">
-              I am Savañ
-            </h2>
-            <h3
-              className={`text-2xl py-2 md:text-3xl ${
-                darkMode ? "text-white" : ""
-              }`}
-            >
-              <span
-                className={`text-teal-500 ${darkMode ? "text-gray-300" : ""}`}
-              >
-                A Designer/Developer
-              </span>
-              .
-            </h3>{" "}
-            <p className="text-md py-5 leading-8 text-gray-800 dark:text-gray-300 md:text-xl max-w-lg mx-auto">
-              I'm Developer with an eye of bringing products to life.
-            </p>
-          </div>
-          <div className="text-5xl flex justify-center gap-16 py-3 text-gray-600 dark:text-gray-400">
-            <AiFillGithub />
-            <AiFillLinkedin />
-          </div>
-          <div className="relative mx-auto bg-gradient-to-b from-teal-500 rounded-full h-80 w-80 mt-20 overflow-hidden md:h-96 md:w-96">
-            <Image src={profileImg} fill="true" objectFit="cover" />
+          <div className="block md:flex md:justify-between items-center">
+            <div>
+              <div className="text-center p-10 md:text-left font-serif">
+                <h2 className="text-5xl py-2 text-teal-500 font-medium md:text-6xl">
+                  Hi. I am Savañ
+                </h2>
+                <h3
+                  className={`text-2xl py-2 md:text-3xl ${
+                    darkMode ? "text-white" : ""
+                  }`}
+                >
+                  <span
+                    className={`text-teal-500 ${
+                      darkMode ? "text-gray-300" : ""
+                    }`}
+                  >
+                    A Designer/Developer.
+                  </span>
+                </h3>{" "}
+                <p className="text-md py-5 leading-8 text-gray-500 dark:text-gray-300 md:text-md max-w-lg mx-auto">
+                  Illuminating the Developer's Eye: Bringing Products to Life
+                </p>
+              </div>
+            </div>
+            <div>
+              <Eye />
+            </div>
           </div>
         </section>
         <section>
           <div>
-            <h3 className="text-3xl py-1">Service I Offer</h3>
-            <p className="text-md py-2 leading-8 text-gray-800 dark:text-gray-300">
-              I am full stack Developer with 2 years experience. Also, I am an
-              expert in native and cross-platform mobile applications. I have
-              experience as{" "}
-              <span className="text-teal-500">Full stack developer</span>.
-            </p>
-            <p className="text-md py-2 leading-8 text-gray-800 dark:text-gray-300">
-              I have strong communication skills and I am adaptable and fast
-              learner.
-            </p>
+            <h3 className="text-3xl py-1 font-bold text-gray-800 dark:text-white">
+              Services I Offer
+            </h3>
+            <div className="bg-gray-100 dark:bg-gray-800 rounded-lg p-4 mt-4">
+              <p className="text-md py-2 leading-6 text-gray-800 dark:text-gray-300">
+                With 2 years of experience, I am a versatile full-stack
+                developer proficient in developing native and cross-platform
+                mobile applications.
+              </p>
+              <p className="text-md py-2 leading-6 text-gray-800 dark:text-gray-300">
+                As a full-stack developer, I offer end-to-end solutions for web
+                and mobile applications, combining technical expertise with
+                strong communication skills.
+              </p>
+              <p className="text-md py-2 leading-6 text-gray-800 dark:text-gray-300">
+                With a passion for learning and adapting to new technologies, I
+                consistently deliver high-quality work and excel in dynamic
+                environments.
+              </p>
+            </div>
           </div>
           <div className="lg:flex gap-10">
             <div className="shadow-lg text-center p-10 rounded-xl my-10 dark:bg-gray-800 dark:text-white">
@@ -85,54 +119,63 @@ export default function Home() {
                 src={profileImg}
                 width={100}
                 height={100}
-                className="mx-auto"
+                className="mx-auto rounded-full"
               />
-              <h3 className="text-lg font-medium pt-8 pb-2">
-                Beautiful Designs
-              </h3>
+              <h3 className="text-lg font-medium pt-8 pb-2">Web Development</h3>
               <p className="py-2">
-                Creating beautiful designs for your needs and desires.
+                Crafting exceptional web experiences tailored to your unique
+                needs.
               </p>
-              <h4 className="py-4 text-teal-600">Design tools I use</h4>
-              <p className="text-gray-800 py-1">React.js</p>
-              <p className="text-gray-800 py-1">Node.js</p>
-              <p className="text-gray-800 py-1">Redux</p>
+              <h4 className="py-4 text-teal-600 font-medium">
+                Technologies I use
+              </h4>
+              <ul className="text-gray-800">
+                <li className="py-1">TypeScript</li>
+                <li className="py-1">MERN Stack</li>
+                <li className="py-1">Redux</li>
+              </ul>
             </div>
             <div className="shadow-lg text-center p-10 rounded-xl my-10 dark:bg-gray-800 dark:text-white">
               <Image
                 src={profileImg}
                 width={100}
                 height={100}
-                className="mx-auto"
+                className="mx-auto rounded-full"
               />
               <h3 className="text-lg font-medium pt-8 pb-2">
-                Beautiful Designs
+                Mobile App Development
               </h3>
               <p className="py-2">
-                Creating beautiful designs for your needs and desires.
+                Creating intuitive and engaging mobile applications for iOS and
+                Android.
               </p>
-              <h4 className="py-4 text-teal-600">Design tools I use</h4>
-              <p className="text-gray-800 py-1">React.js</p>
-              <p className="text-gray-800 py-1">Node.js</p>
-              <p className="text-gray-800 py-1">Redux</p>
+              <h4 className="py-4 text-teal-600 font-medium">
+                Technologies I use
+              </h4>
+              <ul className="text-gray-800">
+                <li className="py-1">React Native</li>
+                <li className="py-1">SwiftUI</li>
+                <li className="py-1">Kotlin</li>
+              </ul>
             </div>
             <div className="shadow-lg text-center p-10 rounded-xl my-10 dark:bg-gray-800 dark:text-white">
               <Image
                 src={profileImg}
                 width={100}
                 height={100}
-                className="mx-auto"
+                className="mx-auto rounded-full"
               />
-              <h3 className="text-lg font-medium pt-8 pb-2">
-                Beautiful Designs
-              </h3>
+              <h3 className="text-lg font-medium pt-8 pb-2">UI/UX Design</h3>
               <p className="py-2">
-                Creating beautiful designs for your needs and desires.
+                Crafting visually appealing and user-friendly interfaces that
+                drive engagement.
               </p>
-              <h4 className="py-4 text-teal-600">Design tools I use</h4>
-              <p className="text-gray-800 py-1">React.js</p>
-              <p className="text-gray-800 py-1">Node.js</p>
-              <p className="text-gray-800 py-1">Redux</p>
+              <h4 className="py-4 text-teal-600 font-medium">Tools I use</h4>
+              <ul className="text-gray-800">
+                <li className="py-1">Figma</li>
+                <li className="py-1">Adobe XD</li>
+                <li className="py-1">Sketch</li>
+              </ul>
             </div>
           </div>
         </section>
