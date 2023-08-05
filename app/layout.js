1800c1e5fceceb385cf20f6cfbad0f00701c6c92
Savan <savansavani111@gmail.com>
2023-08-04 23:55:29 -0400

    google analytics change

--- app/layout.js
+++ app/layout.js
@@ -1,22 +1,34 @@
-import './css/globals.css'
-import { Inter } from 'next/font/google'
+import './css/globals.css';
+import { Inter } from 'next/font/google';
 import { Analytics } from '@vercel/analytics/react';
+import Script from 'next/script';
 
-
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ['latin'] });
 
 export const metadata = {
   title: 'Savan Portfolio',
   description: '',
-}
+};
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      
+      <head>
+        <Script
+          strategy="beforeInteractive"
+          src="https://www.googletagmanager.com/gtag/js?id=G-3LJTTGTYEN"
+        />
+        <Script strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-3LJTTGTYEN');
+          `}
+        </Script>
+      </head>
       <body className={inter.className}>{children}</body>
       <Analytics />
-
     </html>
-  )
+  );
 }
